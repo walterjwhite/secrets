b9612b731d4a3f9e61c1ab39f5e95ebda2373bb1
Walter White <WalterWhite@walterjwhite.com>
2022-06-13 00:00:00 -0400

    merged

--- lib/cmds/find.sh
+++ lib/cmds/find.sh
@@ -2,7 +2,7 @@
 
 _FILTER_ARGS=$(echo "$@" | tr ' ' '|')
 find . -type f -name value |
-	$_GNU_GREP -P "($_FILTER_ARGS)" |
+	$_CONF_GNU_GREP -P "($_FILTER_ARGS)" |
 	sed -e "s/\.\///" |
 	sed -e "s/\/value//" |
-	$_GNU_GREP -P "($_FILTER_ARGS)" --color
+	$_CONF_GNU_GREP -P "($_FILTER_ARGS)" --color
